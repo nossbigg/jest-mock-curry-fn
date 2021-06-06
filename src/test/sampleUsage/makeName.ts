efa8c5defcc329318763efbc59dd2b670e9087ee
nossbigg <chenggibson@gmail.com>
2021-06-06 22:01:06 +0800

    Fix types in /test/sampleUsage

--- src/test/sampleUsage/makeName.ts
+++ src/test/sampleUsage/makeName.ts
@@ -1,6 +1,6 @@
 import { stringPrefixer } from './stringPrefixer'
 
-export const makeName = (name) => {
+export const makeName = (name: string) => {
   const prefix = stringPrefixer('first')('second')
   const result = `${prefix} ${name}`
   return result
