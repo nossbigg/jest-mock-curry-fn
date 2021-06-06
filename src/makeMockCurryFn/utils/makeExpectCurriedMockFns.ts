e1f848d3716930ef1b4a1dfc654c74028459c438
nossbigg <chenggibson@gmail.com>
2021-06-06 21:59:51 +0800

    Fix expectMockFnsCalledWith()

--- src/makeMockCurryFn/utils/makeExpectCurriedMockFns.ts
+++ src/makeMockCurryFn/utils/makeExpectCurriedMockFns.ts
@@ -5,11 +5,26 @@ export const makeExpectCurriedMockFns = (
 ) => (...expectedFnArgs: any[]) => {
   const { mockFns, index } = setup
 
-  if (index >= mockFns.length - 1) {
+  doMatchedMockFnAssertion(setup, expectedFnArgs)
+
+  const shouldReturnCurriedFn = index < mockFns.length - 1
+  if (!shouldReturnCurriedFn) {
     return undefined
+  }
+  return makeExpectCurriedMockFns({ ...setup, index: index + 1 })
+}
+
+const doMatchedMockFnAssertion = (
+  setup: MakeExpectCurriedMockFnsSetup,
+  expectedFnArgs: any[]
+): void => {
+  const { mockFns, index } = setup
+
+  const isWithinIndex = index < mockFns.length
+  if (!isWithinIndex) {
+    return
   }
 
   const matchedMockFn = mockFns[index]
   expect(matchedMockFn).toHaveBeenCalledWith(...expectedFnArgs)
-  return makeExpectCurriedMockFns({ ...setup, index: index + 1 })
 }
